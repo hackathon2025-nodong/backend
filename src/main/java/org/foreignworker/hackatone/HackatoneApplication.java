61005fde330708c7713f2459e1ff7880efbea3b8
summit1123 <ruggy245@naver.com>
2025-04-03 19:19:48 +0900

    chore : build default config

---
+++ src/main/java/org/foreignworker/hackatone/HackatoneApplication.java
@@ -0,0 +1,13 @@
+package org.foreignworker.hackatone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/foreignworker/hackatone/HackatoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foreignworker.hackatone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
