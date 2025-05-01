70a622b999b6fa35830b89d0457b7173e24683e8
JiHo Yu <yujiho568@gmail.com>
2025-05-01 10:41:40 +0900

    feat:user 내부도메인 추가

--- src/main/java/org/foreignworker/hackatone/domain/user/repository/UserRepository.java
+++ src/main/java/org/foreignworker/hackatone/domain/user/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package org.foreignworker.hackatone.domain.user.repository;
 
 import org.foreignworker.hackatone.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
